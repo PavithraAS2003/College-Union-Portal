c94d61fbb6c18063249f2e9ea8e254bdd69b67ce
Neha Arun <105504061+Neha-X-code@users.noreply.github.com>
2024-05-02 03:45:47 +0530

    front end

--- events.php
+++ events.php
@@ -161,34 +161,7 @@
                 <h4 class="card-title"> Simple Table</h4>
               </div>
               <div class="card-body">
-                <div class="container">
-                  <div class="page-header">
-                    <div class="pull-right form-inline">
-                      <div class="btn-group">
-                        <button class="btn btn-primary" data-calendar-nav="prev"><< Prev</button>
-                        <button class="btn btn-default" data-calendar-nav="today">Today</button>
-                        <button class="btn btn-primary" data-calendar-nav="next">Next >></button>
-                      </div>
-                      <div class="btn-group">
-                        <button class="btn btn-warning" data-calendar-view="year">Year</button>
-                        <button class="btn btn-warning active" data-calendar-view="month">Month</button>
-                        <button class="btn btn-warning" data-calendar-view="week">Week</button>
-                        <button class="btn btn-warning" data-calendar-view="day">Day</button>
-                      </div>
-                    </div>
-                    <h3></h3>
-                    <small>To see example with events navigate to February 2018</small>
-                  </div>
-                  <div class="row">
-                    <div class="col-md-9">
-                      <div id="showEventCalendar"></div>
-                    </div>
-                    <div class="col-md-3">
-                      <h4>All Events List</h4>
-                      <ul id="eventlist" class="nav nav-list"></ul>
-                    </div>
-                  </div>
-                </div>
+
                   
               </div>
             </div>
