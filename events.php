04ba103feb44cb3402f8d0d4e9f82de634cc5209
Neha Arun <105504061+Neha-X-code@users.noreply.github.com>
2024-07-09 12:56:13 +0530

    frontend working

--- events.php
+++ events.php
@@ -164,29 +164,12 @@ require 'connect.php';
       </div>
       <footer class="footer">
         <div class="container-fluid">
-          <ul class="nav">
-            <li class="nav-item">
-              <a href="javascript:void(0)" class="nav-link">
-                Creative Tim
-              </a>
-            </li>
-            <li class="nav-item">
-              <a href="javascript:void(0)" class="nav-link">
-                About Us
-              </a>
-            </li>
-            <li class="nav-item">
-              <a href="javascript:void(0)" class="nav-link">
-                Blog
-              </a>
-            </li>
-          </ul>
           <div class="copyright">
             ©
             <script>
               document.write(new Date().getFullYear())
             </script>2018 made with <i class="tim-icons icon-heart-2"></i> by
-            <a href="javascript:void(0)" target="_blank">Creative Tim</a> for a better web.
+            <a href="javascript:void(0)" target="_blank">Team Avenir</a> for College Union LBSITW.
           </div>
         </div>
       </footer>
